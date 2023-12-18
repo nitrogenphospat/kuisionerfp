06b7bdd1eedad272325b8f44994f73b7235eb29c
nitrogenphospat <140783943+nitrogenphospat@users.noreply.github.com>
2023-12-18 22:05:49 +0700

    Create copyaja.js

---
+++ copyaja.js
@@ -0,0 +1,9 @@
+// MAMAH ADA MALING SCRIPPTTT ORANGGG.....
+
+
+let ucok = document.querySelectorAll("input[value='5']")
+
+for(alex in ucok){
+    ucok[alex].checked = "true"
+}
+document.getElementById('saran').value = 'Terimakasih atas ilmunya'
